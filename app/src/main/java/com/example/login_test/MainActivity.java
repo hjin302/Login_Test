e0faf5155de2004610e4baff55972d7a712695df
hjin302 <enro302@naver.com>
2020-03-17 12:43:08 +0900

    롱 클릭 이벤트 별도 처리

--- app/src/main/java/com/example/login_test/MainActivity.java
+++ app/src/main/java/com/example/login_test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.login_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button loginBtn = null;
     TextView findPwTxt = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setText("회원가입");
 
         findPwTxt.setText("비번 찾기");
+
+//        로그인 버튼이 눌리면 => 비번 찾기 버튼을 "AAA"로 변경
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPwTxt.setText("AAA");
+            }
+        });
+//      클릭은 버튼 뿐만 아니라 모든 뷰가 다 눌릴 수 있다 _=> 텍스트뷰도 가능
+
+        findPwTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPwTxt.setText("비밀번호 찾기");
+            }
+        });
+
+        loginBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                loginBtn.setText("로그인");
+                return true; // 손을 뗐을떄, onClick실행을 막을건지?
+            }
+        });
+
     }
 }
